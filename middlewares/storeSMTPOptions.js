210155b66f68623a0937064acdd12ebcf586e8be
Alfayed Dennita <alfayeddennita@gmail.com>
2023-12-04 10:21:16 +0700

    Fix typo

--- middlewares/storeSMTPOptions.js
+++ middlewares/storeSMTPOptions.js
@@ -1,7 +1,7 @@
 function storeSMTPOptions(req, res, next) {
   req.smtpOptions = {
     host: req.headers['x-smtp-host'],
-    port: req.headers['x-smtp-host'],
+    port: req.headers['x-smtp-port'],
     username: req.headers['x-smtp-username'],
     password: req.headers['x-smtp-password'],
   };
